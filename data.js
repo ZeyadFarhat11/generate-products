646035d7aa785a73e099766900f7aac86be35861
ZeyadFarhat11 <zeyadfarhat145@gmail.com>
2023-11-12 20:17:54 +0200

    Initial commit

---
+++ data.js
@@ -0,0 +1,303 @@
+export const REVIEWS = [
+  {
+    username: "jennifer",
+    text: "I'm loving this product! It exceeded my expectations. Highly recommended.",
+    rating: 5,
+  },
+  {
+    username: "alex",
+    text: "Not bad, but could be better. It's just okay.",
+    rating: 3,
+  },
+  {
+    username: "sarah",
+    text: "Absolutely fantastic! This product is a game-changer.",
+    rating: 5,
+  },
+  {
+    username: "john",
+    text: "Disappointed with the quality. It didn't meet my expectations.",
+    rating: 2,
+  },
+  {
+    username: "emily",
+    text: "Great value for the price. I'm very satisfied with my purchase.",
+    rating: 4,
+  },
+  {
+    username: "david",
+    text: "Poor customer service. The company needs to improve their support.",
+    rating: 2,
+  },
+  {
+    username: "lisa",
+    text: "This product is a lifesaver. It solved my problem instantly.",
+    rating: 5,
+  },
+  {
+    username: "michael",
+    text: "Not impressed. The product didn't live up to the hype.",
+    rating: 2,
+  },
+  {
+    username: "natalie",
+    text: "I can't imagine my life without this product. It's amazing!",
+    rating: 5,
+  },
+  {
+    username: "peter",
+    text: "Average product. Nothing special about it.",
+    rating: 3,
+  },
+  {
+    username: "olivia",
+    text: "The best product I've ever used. I'm blown away by its performance.",
+    rating: 5,
+  },
+  {
+    username: "william",
+    text: "Waste of money. I regret buying this product.",
+    rating: 1,
+  },
+  {
+    username: "samantha",
+    text: "Highly disappointed. The product arrived damaged.",
+    rating: 1,
+  },
+  {
+    username: "james",
+    text: "Decent product, but there are better alternatives available.",
+    rating: 3,
+  },
+  {
+    username: "emma",
+    text: "I'm in love with this product. It exceeded my expectations.",
+    rating: 5,
+  },
+  {
+    username: "ryan",
+    text: "Not worth the price. I expected better quality.",
+    rating: 2,
+  },
+  {
+    username: "ava",
+    text: "This product is a game-changer. I can't recommend it enough.",
+    rating: 5,
+  },
+  {
+    username: "matthew",
+    text: "The product is good, but the customer service needs improvement.",
+    rating: 4,
+  },
+  {
+    username: "mia",
+    text: "I'm extremely satisfied with this product. It's worth every penny.",
+    rating: 5,
+  },
+  {
+    username: "ethan",
+    text: "Below average. It didn't meet my expectations.",
+    rating: 2,
+  },
+  {
+    username: "chloe",
+    text: "This product is a disappointment. I regret buying it.",
+    rating: 1,
+  },
+  {
+    username: "daniel",
+    text: "Impressive product with excellent quality. Highly recommended.",
+    rating: 5,
+  },
+  {
+    username: "grace",
+    text: "The product is average. It does the job, but nothing extraordinary.",
+    rating: 3,
+  },
+  {
+    username: "jackson",
+    text: "This product is a complete waste of money. I'm highly dissatisfied.",
+    rating: 1,
+  },
+  {
+    username: "harper",
+    text: "I'm pleasantly surprised by this product. It exceeded my expectations.",
+    rating: 4,
+  },
+  {
+    username: "aiden",
+    text: "Not impressed. The product didn't live up to the hype.",
+    rating: 2,
+  },
+  {
+    username: "sophia",
+    text: "This product is amazing. It's a game-changer for me.",
+    rating: 5,
+  },
+  {
+    username: "logan",
+    text: "I expected better. The product didn't deliver as promised.",
+    rating: 2,
+  },
+  {
+    username: "abigail",
+    text: "I'm extremely satisfied with this product. It's worth every penny.",
+    rating: 5,
+  },
+  {
+    username: "lucas",
+    text: "Disappointing product. It didn't meet my expectations.",
+    rating: 2,
+  },
+  {
+    username: "amelia",
+    text: "This product is a game-changer. I can't imagine my life without it.",
+    rating: 5,
+  },
+  {
+    username: "carter",
+    text: "Average product. It does the job, but nothing exceptional.",
+    rating: 3,
+  },
+  {
+    username: "mia",
+    text: "The product is terrible. I regret buying it.",
+    rating: 1,
+  },
+  {
+    username: "sebastian",
+    text: "This product exceeded my expectations. I'm impressed with its performance.",
+    rating: 5,
+  },
+  {
+    username: "violet",
+    text: "Not worth the price. I expected better quality.",
+    rating: 2,
+  },
+  {
+    username: "henry",
+    text: "I'm loving this product! It has made my life so much easier.",
+    rating: 5,
+  },
+  {
+    username: "lily",
+    text: "Decent product. It gets the job done.",
+    rating: 3,
+  },
+  {
+    username: "dylan",
+    text: "I'm highly disappointed with this product. It didn't meet my expectations.",
+    rating: 1,
+  },
+  {
+    username: "zoe",
+    text: "I can't recommend this product enough. It's a must-have.",
+    rating: 5,
+  },
+  {
+    username: "jacob",
+    text: "Average product, nothing special about it.",
+    rating: 3,
+  },
+  {
+    username: "hannah",
+    text: "This product is a lifesaver. It solved my problem instantly.",
+    rating: 5,
+  },
+  {
+    username: "wyatt",
+    text: "I expected better quality. The product feels cheap.",
+    rating: 2,
+  },
+  {
+    username: "grace",
+    text: "Impressive product with excellent performance. Highly recommended.",
+    rating: 5,
+  },
+  {
+    username: "noah",
+    text: "Disappointed with this product. It didn't meet my expectations.",
+    rating: 2,
+  },
+  {
+    username: "scarlett",
+    text: "This product is amazing. It exceeded my expectations.",
+    rating: 5,
+  },
+  {
+    username: "samuel",
+    text: "Average product. It does the job but lacks innovation.",
+    rating: 3,
+  },
+  {
+    username: "aubrey",
+    text: "This product is a waste of money. I regret buying it.",
+    rating: 1,
+  },
+  {
+    username: "benjamin",
+    text: "I'm pleasantly surprised by this product. It's better than expected.",
+    rating: 4,
+  },
+  {
+    username: "sophie",
+    text: "Not impressed. The product didn't live up to the hype.",
+    rating: 2,
+  },
+  {
+    username: "joseph",
+    text: "Great value for the price. I'm very satisfied with my purchase.",
+    rating: 4,
+  },
+  {
+    username: "mia",
+    text: "This product is a disappointment. It didn't meet my expectations.",
+    rating: 1,
+  },
+  {
+    username: "leo",
+    text: "I'm extremely impressed with this product. It's worth every penny.",
+    rating: 5,
+  },
+  {
+    username: "madison",
+    text: "Average product. It does the job but lacks innovation.",
+    rating: 3,
+  },
+  {
+    username: "david",
+    text: "This product is terrible. I regret buying it.",
+    rating: 1,
+  },
+];
+
+export const DESCRIPTIONS = {
+  adidas:
+    "The Adidas Ultraboost shoes offer an incredibly comfortable fit, with their cushioned sole and breathable upper. The only downside was that the laces were a bit too long and required double knotting, but overall, these shoes are a top-notch choice for running enthusiasts.",
+  converse:
+    "The Converse Chuck Taylor All Stars are timeless classics that never disappoint. They provide a snug and comfortable fit, and their sleek design goes well with any outfit. The only minor issue was that the rubber sole tends to wear out quickly, but the overall quality and style make them a must-have in any sneaker collection.",
+  nike: "The Nike React Element 55 sneakers are the epitome of style and comfort. The lightweight construction and responsive cushioning make them perfect for all-day wear. The only drawback was that the sizing runs a bit small, so it's advisable to go half a size up. Nevertheless, these shoes are a game-changer in terms of both looks and performance.",
+  puma: "The Puma Suede Classic shoes are a true embodiment of retro coolness. They offer a comfortable fit with their padded collar and soft suede upper. The only downside was that the outsole tends to be a bit slippery on wet surfaces, but the iconic design and versatility of these sneakers make them a go-to choice for casual and streetwear fashion.",
+  vans: "The Vans Old Skool sneakers are a timeless icon in the world of skateboarding and street fashion. Their sturdy canvas upper and signature side stripe provide durability and style. The only minor issue was that the insole could use a bit more arch support for long hours of wear, but overall, these shoes are a classic staple that never goes out of style.",
+};
+
+export const TITLES = {
+  nike: "Nike Air Max React",
+  adidas: "Adidas New Hero Max",
+  converse: "Converse Chuck Taylor All Star",
+  puma: "Puma Shox Vulcan",
+  vans: "Vans Timu Lara",
+};
+export const RATINGS = [3, 3.5, 3.9, 4, 4.2, 4.5, 4.8];
+export const IMGS = [
+  "image-src-1",
+  "image-src-2",
+  "image-src-3",
+  "image-src-4",
+  "image-src-5",
+];
+export const BRANDS = ["Adidas", "Converse", "Nike", "Puma", "Vans"];
+export const CATEGORIES = ["Shoes", "T-shirts", "Bags", "Jackets"];
+export const COLORS = ["red", "blue", "yellow", "violet", "black"];
+
+export const SIZES = ["SM", "MD", "LG", "XL", "2XL"];
---
+++ index.js
@@ -0,0 +1,77 @@
+import _ from "lodash";
+import {
+  BRANDS,
+  DESCRIPTIONS,
+  IMGS,
+  CATEGORIES,
+  COLORS,
+  RATINGS,
+  TITLES,
+  REVIEWS,
+  SIZES,
+} from "./data.js";
+import fs from "fs";
+
+const generateProduct = () => {
+  const imgs = _.sampleSize(IMGS, 4);
+  const brand = _.sample(BRANDS);
+  const reviews = _.sampleSize(REVIEWS, Math.floor(Math.random() * 40));
+  const product = {
+    title: TITLES[brand.toLocaleLowerCase()],
+    rating: _.sample(RATINGS),
+    price: Math.floor(Math.random() * 900) + 100,
+    img: imgs[0],
+    imgs: imgs.slice(1),
+    discount: Math.floor(Math.random() * 10) + 15,
+    brand,
+    category: _.sample(CATEGORIES),
+    freeShipping: Math.random() > 0.5,
+    colors: Array(Math.floor(Math.random() * 3) + 1)
+      .fill(0)
+      .map(() => _.sample(COLORS)),
+    sizes: SIZES.slice(0, Math.floor(Math.random() * SIZES.length) + 2),
+    available: Math.random() > 0.1,
+    label: Math.random() > 0.8 ? "HOT" : undefined,
+    reviews,
+    reviewsCount: reviews.length,
+    description: DESCRIPTIONS[brand.toLocaleLowerCase()],
+
+    active: true,
+  };
+
+  return product;
+};
+
+function getLastNumberInFileName() {
+  const files = fs.readdirSync("./"); // Assuming the files are in the current directory
+
+  let lastNumber = 0;
+
+  for (const file of files) {
+    if (file.startsWith("products-") && file.endsWith(".json")) {
+      const number = parseInt(file.match(/products-(\d+)\.json/)[1]);
+      if (number > lastNumber) {
+        lastNumber = number;
+      }
+    }
+  }
+
+  return lastNumber;
+}
+
+const wantedProductsNumber = parseInt(process.argv[2]);
+
+if (typeof wantedProductsNumber !== "number") {
+  throw new Error("Invalid products number");
+}
+
+let products = [];
+for (let i = 0; i < wantedProductsNumber; i++) {
+  products.push(generateProduct());
+}
+fs.writeFileSync(
+  `./products-${getLastNumberInFileName() + 1}.json`,
+  JSON.stringify(products),
+  "utf-8"
+);
+console.log("Products Generated Successfully ✅");
